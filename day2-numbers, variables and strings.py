77f0be7571841c7773992a1f32c47e50548e5e9e
baruch <lb@LBs-MacBook-Air.home>
2022-02-12 00:09:50 -0500

    numbers, variables and strings

---
+++ day2-numbers, variables and strings.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+Numbers / Integers
+
+Adding
+
+10 + 10
+
+Multiply
+
+9 * 32
+
+Divide
+
+24 / 6
+
+Exponent
+
+4 ** 2
+
+Modulo (Remainder)
+
+24 % 5
+
+Greater Than
+
+55 > 22
+
+Less Than
+
+132 < 123
+
+Syntax Errors
+
+>>> Hello World
+SyntaxError: invalid syntax
