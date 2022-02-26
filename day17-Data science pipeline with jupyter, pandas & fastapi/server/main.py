711a3fc8d2e01e72a7fba502616b6ed379076ba0
baruch <arbaherpe07@hotmail.com>
2022-02-26 02:48:19 -0500

    Data science pipeline with jupyter, pandas & fastapi  day17

---
+++ day17-Data science pipeline with jupyter, pandas & fastapi/server/main.py
@@ -0,0 +1,16 @@
+import os
+from fastapi import FastAPI
+import pandas as pd
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CACHE_DIR = os.path.join(BASE_DIR, 'cache')
+dataset = os.path.join(CACHE_DIR, 'movies-box-office-dataset-cleaned.csv')
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/box-office")
+def read_box_office_numbers():
+    df = pd.read_csv(dataset)
+    return df.to_dict("Rank")
